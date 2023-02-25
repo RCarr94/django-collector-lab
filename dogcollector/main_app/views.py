3c442ef39194da51a16a6ae88b37b8da1df9469b
Ryan Carr <ryan.g.carr@hotmail.com>
2023-02-25 16:25:27 -0500

    add dog model and superuser

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,21 +1,5 @@
-# TODO: remove this, for testing only --> model coming later
-
-class Dog: 
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-dogs = [
-    Dog('Luna', 'Shepherd', 'smart, friendly, cuddly', 2),
-    Dog('Nova', 'Retriever-Mix', 'shy, cuddly, playful', 1),
-]
-
-
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from .models import Dog
 
 # Home View
 def home(request):
@@ -25,4 +9,5 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', { 'dogs': dogs })
