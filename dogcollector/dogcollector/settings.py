f3c7af3662cfd02834d4051508de06edf29d80d7
Ryan Carr <ryan.g.carr@hotmail.com>
2023-03-15 21:45:10 -0400

    start heroku prep

--- dogcollector/dogcollector/settings.py
+++ dogcollector/dogcollector/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+WSGI_APPLICATION = 'dogcollector.wsgi.application'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
